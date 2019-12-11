1e24551749913eaead0e952b360b1b67d8f39b0f
wulikunkun <wulikunkun@outlook.com>
2019-12-11 10:57:00 +0800

    完成搜索选择插件的绝大部分功能，并从CNKI的解决方案中引入jquery1.7.1

--- src/searchSelect/searchSelect.js
+++ src/searchSelect/searchSelect.js
@@ -1,24 +1,22 @@
 (function(jQuery, window, document, undefined) {
   function SearchSelect(ele, options) {
-    var self = this;
-    self.$ele = $(ele);
-    self.defaults = {
+    this.$ele = $(ele);
+    this.defaults = {
       data: []
     };
-
-    self.components = {
+    this.components = {
       $dropdownContainer: $(
         "<div style='display:inline-block;position:relative;'></div>"
       ),
       $dropdown: $(
-        "<div style='border:1px solid lightgray;border-radius:3px;display:none'></div>"
+        "<div style='border:1px solid lightgray;border-radius:3px;display:none' class='cnki_wulikunkun_dropdown'></div>"
       ),
-      $searchInput: $(
-        "<input type='search' style='width:100%;padding:3px 2px'></input>"
+      $noResultAlert: $(
+        "<option style='cursor:pointer;padding:2px 10px;text-align:left'>没有搜索到对应学校！</option>"
       )
     };
-    self.settings = $.extend({}, self.defaults, options);
-    self.init();
+    this.settings = $.extend({}, this.defaults, options);
+    this.init();
   }
 
   SearchSelect.prototype = {
@@ -41,62 +39,104 @@
       //   jquery中的wrap方法是将参数中的元素包裹到调用wrap方法的元素的外面;
       this.$ele.wrap(this.components.$dropdownContainer);
       this.$ele.after(this.components.$dropdown);
-      //   this.components.$dropdown.prepend(this.components.$searchInput);
       this.components.$dropdown.width(this.$ele.innerWidth());
 
       for (var i = 0; i < this.settings.data.length; i++) {
         this.components.$dropdown.append(
-          "<option style='cursor:pointer;padding:2px 10px;text-align:left'>" +
+          "<option style='cursor:pointer;padding:3px 10px;text-align:left'>" +
             this.settings.data[i] +
             "</option>"
         );
       }
     },
     initEvents: function() {
+      var self = this;
+
       this.$ele.on("focus", $.proxy(this.showPanel, this));
       this.$ele.on("blur", $.proxy(this.hidePanel, this));
+
       this.$ele.bind(
         "input propertychange change",
         $.proxy(this.filterOptions, this)
       );
+
+      this.$ele.parent().on("click", "option", $.proxy(this.selectItem, this));
+
       this.$ele
         .parent()
-        .find("option")
-        .on("click", $.proxy(this.selectItem, this));
+        .on("mouseover", "option", $.proxy(this.changeItemBackcolor, this));
+
+      this.$ele
+        .parent()
+        .on(
+          "mouseout",
+          "option",
+          $.proxy(this.changeItemBackcolorToOriginal, this)
+        );
+
+      // 比较这里在一个function中调用proxy和上面直接代用proxy的区别
+      this.components.$dropdown.on("mouseover", function() {
+        self.$ele.unbind("blur", $.proxy(self.hidePanel, self));
+      });
+
+      this.components.$dropdown.on("mouseout", function() {
+        self.$ele.bind("blur", $.proxy(self.hidePanel, self));
+      });
+
+      // $(document).on("click", $.proxy(this.hidePanel, this));
+      // $(document).on("click", ":not(.cnki_wulikunkun_dropdown)", function() {
+      //   alert("触发");
+      // });
     },
     showPanel: function() {
       this.components.$dropdown.show();
     },
     hidePanel: function() {
+      debugger;
       this.components.$dropdown.hide();
     },
     filterOptions: function() {
+      this.components.$noResultAlert.remove();
+
       var searchContent = this.$ele.val();
       var filteredData = [];
       for (var i = 0; i < this.settings.data.length; i++) {
         if (this.settings.data[i].indexOf(searchContent) > -1) {
           filteredData.push(this.settings.data[i]);
         }
-        this.components.$dropdown.find("option").remove();
+
+        this.$ele
+          .parent()
+          .find("option")
+          .each(function(index, item) {
+            if (
+              filteredData.indexOf(
+                $(item)
+                  .text()
+                  .trim()
+              ) == -1
+            ) {
+              $(item).hide();
+            } else {
+              $(item).show();
+            }
+          });
       }
       if (filteredData.length == 0) {
-        this.components.$dropdown.append(
-          "<option style='cursor:pointer;padding:2px 10px;text-align:left'>没有搜索到对应学校！</option>"
-        );
-      } else {
-        for (var i = 0; i < filteredData.length; i++) {
-          this.components.$dropdown.append(
-            "<option style='cursor:pointer;padding:2px 10px;text-align:left'>" +
-              filteredData[i] +
-              "</option>"
-          );
-        }
+        this.components.$dropdown.append(this.components.$noResultAlert);
       }
     },
-    selectItem: function() {
-      console.log(this);
-      alert("触发!");
+    selectItem: function(e) {
+      this.$ele.val($(e.target).text());
+      this.hidePanel();
       debugger;
+      this.$ele.bind("blur", $.proxy(this.hidePanel, this));
+    },
+    changeItemBackcolor: function(e) {
+      $(e.target).css("background-color", "lightgray");
+    },
+    changeItemBackcolorToOriginal: function(e) {
+      $(e.target).css("background-color", "white");
     }
   };
 
@@ -107,5 +147,6 @@
   };
 })(jQuery, document, window);
 
-// Author info:wulikunkun
+// Author info:Wang Kun
 // Date: 2019-12-10-13-33-04
+// Dependency: jQuery 1.7.0
