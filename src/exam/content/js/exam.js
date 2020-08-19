6b6413e9704e26873b596f937d2da28091d10904
王昆 <5627379+wulikunkun@user.noreply.gitee.com>
2020-08-19 17:04:16 +0800

    临时迁入

--- src/exam/content/js/exam.js
+++ src/exam/content/js/exam.js
@@ -28,7 +28,7 @@
       ),
       $leftPanelNavContainer: $('<nav class="nav-bar"></nav>'),
       $rightPanel: $(
-        '<div class="col-9 bg-light vh-100 overflow-auto position-relative px-5"><div class="card min-vh-100 my-5 rounded-0 p-5 border-0 shadow-sm mx-auto" style="width:210mm;min-width:210mm"></div> </div>'
+        '<div class="col-9 bg-light vh-100 overflow-auto position-relative px-5"><div class="card min-vh-100 rounded-0 border-0 shadow-sm mx-auto" style="width:210mm;min-width:210mm"></div> </div>'
       ),
     };
 
--- src/reader/content/js/reader.js
+++ src/reader/content/js/reader.js
@@ -13,22 +13,14 @@
 
     this.components = {
       $container: $(
-        "<div class='container-fluid'><div class='row' id='container'></div></div>"
+        "<div class='container-fluid'><div class='row' id='container'><div class='col-2'></div></div></div>"
       ),
       $leftPanel: $(
-        '<div class="col-3 vh-100 overflow-auto position-relative px-0 shadow-sm" style="background-color: #333;"></div>'
-      ),
-      $leftPanelTopBar: $(
-        '<div class="w-100 px-4 text-white py-3 border-bottom border-dark"><i class="fa fa-angle-left" aria-hidden="true"></i><a class="float-right text-light font-weight-bold" href="#">返 回</a></div>'
-      ),
-      $leftPanelCover: $(
-        '<img src="' +
-          this.settings.coverUrl +
-          '" class="w-50 d-block p-2 mx-auto my-2" />'
+        '<div class="col-2 vh-100 overflow-auto position-relative px-0 shadow-sm bg-white"></div>'
       ),
       $leftPanelNavContainer: $('<nav class="nav-bar"></nav>'),
       $rightPanel: $(
-        '<div class="col-9 bg-light vh-100 overflow-auto position-relative px-5"><div class="card min-vh-100 my-5 rounded-0 p-5 border-0 shadow-sm mx-auto" style="width:210mm;min-width:210mm"></div> </div>'
+        '<div class="col-6 bg-light vh-100 overflow-auto position-relative px-0"><div class="card min-vh-100 rounded-0 px-5 py-2 border-0 shadow-sm mx-0"></div> </div><div class="col-2"></div>'
       ),
     };
 
@@ -38,7 +30,6 @@
   Reader.prototype = {
     init: function () {
       this.initStyle();
-      this.initEvents();
     },
     initStyle: function () {
       /* js文件在哪个页面中执行资源路径就相对于哪个页面 */
@@ -57,13 +48,12 @@
       var $hTagDoms = $dataDom.filter("h1,h2,h3,h4,h5,h6");
 
       for (var i = 0; i < $hTagDoms.length; i++) {
-        debugger;
         var currentItem = $hTagDoms[i],
           $currentItem = $(currentItem);
         $currentItem.attr("id", $currentItem.text());
         if (currentItem.tagName == "H1") {
           var $nextLevelItem = $(
-            '<a class="nav-link text-light border-top border-dark py-3 font-weight-bold text-left px-4" data-level="1" href="#' +
+            '<a class="nav-link text-muted border-top border-white py-1 font-weight-bold text-left px-4" data-level="1" href="#' +
               $currentItem.text() +
               '">' +
               $currentItem.text() +
@@ -73,56 +63,20 @@
         } else this.GenerateChildLevel(currentItem);
       }
 
-      this.components.$leftPanel
-        .append(this.components.$leftPanelTopBar)
-        .append(this.components.$leftPanelCover);
-
       this.components.$rightPanel.find("div.card").html($dataDom);
 
-      this.components.$leftPanelCover.after(
-        this.components.$leftPanelNavContainer
-      );
-
+      this.components.$leftPanel.append(this.components.$leftPanelNavContainer);
       this.components.$container.children().append(this.components.$leftPanel);
       this.components.$container.children().append(this.components.$rightPanel);
 
       this.$ele.append(this.components.$container);
-    },
-    initEvents: function () {
-      this.components.$leftPanelNavContainer
-        .children()
-        .on("click", $.proxy(this.showOrHideChildLevel, this));
-    },
-    showOrHideChildLevel: function (e) {
-      var $currentLevel = $(e.target);
-      $currentLevel.children("i").toggleClass("fa-angle-right fa-angle-down");
-
-      // this.components.$rightPanel
-      //   .find("iframe")
-      //   .attr("src", "./" + $currentLevel.text().trim() + ".html");
-
-      if ($currentLevel.attr("isshow") == "true") {
-        this.hideChildrenLevel($currentLevel);
-      } else {
-        this.showChildrenLevel($currentLevel);
-      }
-    },
-    showChildrenLevel: function (selectedLevel) {
-      var $selectedLevel = selectedLevel;
-      $selectedLevel.attr("isshow", "true");
-      $selectedLevel.next().slideDown();
-    },
-    hideChildrenLevel: function (selectedLevel) {
-      var $selectedLevel = selectedLevel;
-      $selectedLevel.attr("isshow", "false");
-      $selectedLevel.next("div").slideUp();
     },
     GenerateChildLevel: function (tagItem) {
       var nextLevelNum = tagItem.tagName[1],
         parentLevelNum = nextLevelNum - 1;
 
       var $nextLevelItem = $(
-        '<a class="nav-link text-white-50 border-top border-dark py-2 font-weight-bold text-left px-4 small" href="#' +
+        '<a class="nav-link text-muted border-top border-white py-1 font-weight-bold text-left px-4 small" href="#' +
           $(tagItem).text() +
           '" data-level="' +
           nextLevelNum +
@@ -147,11 +101,10 @@
 
       if ($parentLevel.children("i").length == 0) {
         $parentLevel.append(
-          '&nbsp;&nbsp;<i class="fa fa-angle-right text-white-50" aria-hidden="true"></i>'
+          '&nbsp;&nbsp;<i class="fa fa-angle-right text-muted d-none" aria-hidden="true"></i>'
         );
         var $nextLevelContainer = $("<div></div>");
         $nextLevelContainer.append($nextLevelItem);
-        $nextLevelContainer.hide();
         $parentLevel.after($nextLevelContainer);
       } else {
         $parentLevel.next().append($nextLevelItem);
